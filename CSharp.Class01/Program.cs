9810b16b0e909be0e11a40b96e68e56f6c4a9079
MilchoK <96262988+MilchoK@users.noreply.github.com>
2022-03-10 13:53:44 +0100

    C#Class01-HW

---
+++ CSharp.Class01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSharp.Class01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  * * *");
+            Console.WriteLine("*   *   *");
+            Console.WriteLine("    *");
+            Console.WriteLine("    *");
+            Console.WriteLine("  *   *");
+            Console.WriteLine(" *     *");
+        }
+    }
+}
---
+++ CSharp.Class01/StickMan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StickMan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
