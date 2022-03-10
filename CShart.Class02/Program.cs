979be351d0e5403235d6d50ad47e5088eb2b091d
MilchoK <96262988+MilchoK@users.noreply.github.com>
2022-03-10 14:08:51 +0100

    Class02 HW

---
+++ CShart.Class02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CShart.Class02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            a = 454;
+            b = 26;
+            double resD;
+            resD = a / b;
+            Console.WriteLine(resD);
+            int x, y;
+            x = 52;
+            y = 6;
+            int resI;
+            resI = x / y;
+            Console.WriteLine(resI);
+        }
+    }
+}
---
+++ Exercise04/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m, result;
+            n = 102;
+            m = 5;
+            result = n / m;
+            Console.WriteLine("You can send " + result + " messages");
+        }
+    }
+}
