3618df9cb8e06a746e13323265da7e10af5b4047
MilchoK <96262988+MilchoK@users.noreply.github.com>
2022-03-24 16:48:33 +0100

    Class03 Homeworks

---
+++ CSharp.Class03/StudentGroup/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[] { "Reuben", "Garland", "Luna","Kylee","Chase"};
+            string[] studentsG2 = new string[] { "Jon", "Rob", "Sansa", "Arya", "Michael" };
+
+            Console.WriteLine("Enter student group: ( there are 1 and 2 )");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                foreach (string student in studentsG1)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+            else if (input == "2")
+            {
+                foreach (string student in studentsG2)
+                {
+                    Console.WriteLine(student);
+                }
+            }
+        }
+    }
+}
---
+++ CSharp.Class03/SumOfEven/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Enter integer no.{i + 1}:");
+                string input = Console.ReadLine();
+                int.TryParse(input, out int num);
+                numbers[i] = num;
+            }
+            int sum = 0;
+            foreach (int num in numbers)
+            {
+                if(num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+            Console.WriteLine("The result is: " + sum);
+
+        }
+    }
+}
